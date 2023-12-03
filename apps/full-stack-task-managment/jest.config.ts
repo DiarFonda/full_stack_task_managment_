f0bb4bbbca5564ab392ffd09462ee157aa60cf97
DiarFonda <diarfondaa@gmail.com>
2023-12-03 11:00:11 +0100

    Initial commit with Nx workspace setup

---
+++ apps/full-stack-task-managment/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'full-stack-task-managment',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/full-stack-task-managment',
+};
